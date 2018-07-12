09e14e5774fbb4227cdf664f8656df9aaed28b40
verysuper <verysuper@live.com>
2018-07-12 19:17:12 +0800

    web01_2

--- web01_2/index1.php
+++ web01_2/index1.php
@@ -1,3 +1,6 @@
+<?php
+	include_once('0_config.php');
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
