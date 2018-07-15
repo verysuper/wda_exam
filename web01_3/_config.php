37867ed1e3bae7ba4414a44103f464279290ca00
verysuper <verysuper@live.com>
2018-07-16 02:42:52 +0800

    web01_3

--- web01_3/_config.php
+++ web01_3/_config.php
@@ -5,13 +5,46 @@
     echo $ex->getMessage();
   }
   //index info
-  //totals
+  //1.titles
+  $result=$conn->query("select * from titles where display = 1;");
+  while($row=$result->fetch(PDO::FETCH_ASSOC)){
+    $titles[]=$row;
+  }
+  //2.menu
+
+  //3.totals
   session_start();
   if(!isset($_SESSION['visitor'])){
     $conn->query("update totals set total = total + 1 ;");
     $_SESSION['visitor']=1;
   }
-  //
+  $result=$conn->query("select * from totals;");
+  while($row=$result->fetch(PDO::FETCH_ASSOC)){
+    $totals[]=$row;
+  }
+  
+  //4.ads
+  $result=$conn->query("select * from ads where display = 1;");
+  while($row=$result->fetch(PDO::FETCH_ASSOC)){
+    $ads[]=$row;
+  }
+
+  //5.mvim
+  $result=$conn->query("select * from mvims where display = 1;");
+  while($row=$result->fetch(PDO::FETCH_ASSOC)){
+    $mvims[]=$row;
+  }
+  //echo "<pre>".print_r($mvims,true)."</pre>"; //test
+  foreach( $mvims as $val){
+    echo "<pre>".print_r($val,true)."</pre>"; //test
+  }
+
+  //6.newss
+  $result=$conn->query("select * from newss where display = 1;");
+  while($row=$result->fetch(PDO::FETCH_ASSOC)){
+    $newss[]=$row;
+  }
+  //7.login button
   if(!isset($_SESSION['admin'])){
     $login_text = "管理登入";
 		$login_url = "login.php";
@@ -19,21 +52,15 @@
     $login_text = "回後台管理";
 		$login_url = "admin.php";
   }
+  //8.images
+  $result=$conn->query("select * from images where display = 1;");
+  while($row=$result->fetch(PDO::FETCH_ASSOC)){
+    $images[]=$row;
+  }
 
-  //titles
-  $result=$conn->query("select * from titles where display = 1;");
-  while($row=$result->fetch(PDO::FETCH_ASSOC)){
-    $titles_title=$row['title'];
-    $titles_alt=$row['alt'];
-  }
-  //ads
-  $result=$conn->query("select * from ads where display = 1;");
-  while($row=$result->fetch(PDO::FETCH_ASSOC)){
-    $ads_ads[]=$row['ad'];
-  }
-  //bottoms
+  //9.bottoms
   $result=$conn->query("select * from bottoms;");
   while($row=$result->fetch(PDO::FETCH_ASSOC)){
-    $bottoms_bottom=$row['bottom'];
+    $bottoms=$row;
   }
 ?>
