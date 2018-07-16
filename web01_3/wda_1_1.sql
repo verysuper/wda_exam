34568c83d32c56a940c22c68d23c607e362ea018
verysuper <verysuper@live.com>
2018-07-16 23:38:41 +0800

    web1_3

---
+++ web01_3/wda_1_1.sql
@@ -0,0 +1,315 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機: 127.0.0.1
+-- 產生時間： 2018-07-16 17:38:06
+-- 伺服器版本: 10.1.32-MariaDB
+-- PHP 版本： 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `wda_1_1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `acc` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ps` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 資料表的匯出資料 `admins`
+--
+
+INSERT INTO `admins` (`id`, `acc`, `ps`, `type`) VALUES
+(1, 'admin', '1234', 0),
+(2, '測試1', '1234', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `ads`
+--
+
+CREATE TABLE `ads` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `ad` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `display` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 資料表的匯出資料 `ads`
+--
+
+INSERT INTO `ads` (`id`, `ad`, `display`) VALUES
+(1, '轉知臺北教育大學與臺灣師大合辦第11屆麋研齋全國硬筆書法比賽活動', 1),
+(2, '轉知:法務部辦理「第五屆法規知識王網路闖關競賽辦法', 1),
+(3, '轉知2012年全國青年水墨創作大賽活動', 1),
+(4, '欣榮圖書館101年悅讀達人徵文比賽，歡迎全校師生踴躍投稿參加', 1),
+(5, '轉知:教育是人類升沉的樞紐-2013教師生命成長營', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `bottoms`
+--
+
+CREATE TABLE `bottoms` (
+  `bottom` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `image` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `display` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 資料表的匯出資料 `images`
+--
+
+INSERT INTO `images` (`id`, `image`, `display`) VALUES
+(1, '01D01.jpg', 1),
+(2, '01D02.jpg', 1),
+(3, '01D03.jpg', 1),
+(4, '01D04.jpg', 1),
+(5, '01D05.jpg', 1),
+(6, '01D06.jpg', 1),
+(7, '01D07.jpg', 1),
+(8, '01D08.jpg', 1),
+(9, '01D09.jpg', 1),
+(11, '01D10.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `menus`
+--
+
+CREATE TABLE `menus` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `menu` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `href` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `display` int(1) NOT NULL,
+  `lv` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 資料表的匯出資料 `menus`
+--
+
+INSERT INTO `menus` (`id`, `menu`, `href`, `display`, `lv`) VALUES
+(1, '管理登入', 'login.php', 1, 0),
+(2, '網站首頁', 'index.php', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `mvims`
+--
+
+CREATE TABLE `mvims` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `mvim` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `display` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 資料表的匯出資料 `mvims`
+--
+
+INSERT INTO `mvims` (`id`, `mvim`, `display`) VALUES
+(1, '01C01.swf', 1),
+(2, '01C02.swf', 1),
+(3, '01C03.swf', 1),
+(4, '01C04.swf', 1),
+(5, '01C05.gif', 1),
+(6, '01C06.gif', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `newss`
+--
+
+CREATE TABLE `newss` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `ad` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `display` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 資料表的匯出資料 `newss`
+--
+
+INSERT INTO `newss` (`id`, `ad`, `display`) VALUES
+(1, '教師研習「世界公民生命園丁國內研習會」\r\n1.主辦單位：世界展望會\r\n2.研習日期：101年11月14日（三）～15日（四）\r\n3.詳情請參考：\r\nhttp://gc.worldvision.org.tw/seed.html。\r\n請線上報名。', 1),
+(2, '公告綜合高中一年級英數補救教學時間\r\n上課日期:10/27.11/3.11/10.11/24共計四次\r\n上課時間:早上8:00~11:50半天\r\n費用:全程免費\r\n參加同學:綜合科一年級第一次段考成績需加強者\r\n已將名單送交各班及導師\r\n參加同學請帶紙筆.課本.第一次段考考卷\r\n並將家長通知單給家長\r\n若有任何疑問\r\n請洽綜合高中學程主任', 1),
+(3, '102年全國大專校院運動會\r\n「主題標語及吉祥物命名」\r\n網路票選活動\r\n一、活動期間：自10月25日起至11月4日止。\r\n二、相關訊息請上宜蘭大學首頁連結「102全大運在宜大」\r\n活動網址：http://102niag.niu.edu.tw/', 1),
+(4, '台灣亞洲藝術文化教育交流學會第一屆年會國際研討會\r\n活動日期：101年3月3～4日(六、日)\r\n活動主題：創造力、文化、全人教育\r\n有意參加者請至http://www.caaetaiwan.org下載報名表', 1),
+(5, '11月23日(星期五)將於彰化縣田尾鄉菁芳園休閒農場\r\n舉辦「高中職生涯輔導知能研習」\r\n中區學校每校至多2名\r\n以普通科、專業類科教師優先報名參加\r\n生涯規劃教師次之，參加人員公差假\r\n並核實派代課\r\n當天還有專車接送(8:35前在員林火車站集合)\r\n如此好康的機會，怎能錯過？！\r\n熱烈邀請師長們向輔導室(分機234)報名\r\n名額有限，動作要快！！\r\n報名截止日期：本周四 10月25日17:00前！', 1),
+(6, '台視百萬大明星節目辦理海選活動\r\n時間:101年10月27日下午13時\r\n地點:彰化', 0),
+(7, '台視百萬大明星節目辦理海選活動\r\n時間:101年10月27日下午13時\r\n地點:彰化', 1),
+(8, '國立故宮博物院辦理\r\n「商王武丁與后婦好 殷商盛世文化藝術特展」暨\r\n「赫赫宗周 西周文化特展」\r\n', 1),
+(9, '財團法人漢光教育基金會\r\n辦理2012「舊愛新歡-古典詩詞譜曲創作暨歌唱表演競賽」\r\n參賽獎金豐厚!!\r\n', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `titles`
+--
+
+CREATE TABLE `titles` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alt` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `display` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 資料表的匯出資料 `titles`
+--
+
+INSERT INTO `titles` (`id`, `title`, `alt`, `display`) VALUES
+(1, '01B01.jpg', '卓越科技大學校園資訊系', 1),
+(2, '01B02.jpg', '卓越科技大學校園資訊系', 0),
+(3, '01B03.jpg', '卓越科技大學校園資訊系', 0),
+(4, '01B04.jpg', '卓越科技大學校園資訊系', 0);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `totals`
+--
+
+CREATE TABLE `totals` (
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 資料表的匯出資料 `totals`
+--
+
+INSERT INTO `totals` (`total`) VALUES
+(14);
+
+--
+-- 已匯出資料表的索引
+--
+
+--
+-- 資料表索引 `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `ads`
+--
+ALTER TABLE `ads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `mvims`
+--
+ALTER TABLE `mvims`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `newss`
+--
+ALTER TABLE `newss`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `titles`
+--
+ALTER TABLE `titles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在匯出的資料表使用 AUTO_INCREMENT
+--
+
+--
+-- 使用資料表 AUTO_INCREMENT `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表 AUTO_INCREMENT `ads`
+--
+ALTER TABLE `ads`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- 使用資料表 AUTO_INCREMENT `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- 使用資料表 AUTO_INCREMENT `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表 AUTO_INCREMENT `mvims`
+--
+ALTER TABLE `mvims`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- 使用資料表 AUTO_INCREMENT `newss`
+--
+ALTER TABLE `newss`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- 使用資料表 AUTO_INCREMENT `titles`
+--
+ALTER TABLE `titles`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
