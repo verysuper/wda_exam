d69a8b8d30a40bfa0ed237584015637a163a47bb
verysuper <verysuper@live.com>
2018-07-28 12:11:46 +0800

    web04_1

---
+++ web04_1/_config.php
@@ -0,0 +1,14 @@
+<?php  
+  try{
+    $conn=new PDO("mysql:host=localhost;dbname=wda_4_1;charset=UTF8;","root","");
+  }catch(PDOException $ex){
+    echo $ex->getMessage();
+  }
+  date_default_timezone_set('Asia/Taipei');
+  $now=date('Y/m/d H:i:s',time());
+  //echo $now;
+  session_start();
+  $result=$conn->query("select * from footer");
+  $footer=$result->fetch(PDO::FETCH_ASSOC)['footer'];
+  echo $footer;
+?>
---
+++ web04_1/logout.php
@@ -0,0 +1,5 @@
+<?php
+  session_start();
+  unset($_SESSION['user']);
+?>
+<script>document.location.href='index1.php?do=login'</script>
