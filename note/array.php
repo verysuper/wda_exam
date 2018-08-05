1af1abe4554d6fc0a96cac3e17b69ce489c93e76
verysuper <verysuper@live.com>
2018-08-06 00:38:12 +0800

    web04_2

--- note/array.php
+++ note/array.php
@@ -7,4 +7,8 @@
     echo '<br>';
     $array=array_values($array);
     print_r($array);
+
+    $array = array(0 => "a", 1 => "b", 2 => "c");
+    array_splice($array, 1, 1);
+    print_r($array);
 ?>
