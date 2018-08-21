e2806f0cba87c3121aeaa3b1d37119ae7a4f1bbd
verysuper <verysuper@live.com>
2018-08-21 20:52:30 +0800

    web02_2

--- web02_2/index1.php
+++ web02_2/index1.php
@@ -15,7 +15,7 @@
 <iframe name="back" style="display:none;"></iframe>
 	<div id="all">
     	<div id="title">
-        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
+        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 <a href="index1.php" style="float:right">回首頁</a></div></div>
         <div id="title2">
         	<img src="assets/02B01.jpg" width="100%" alt="健康促進網－回首頁" title="健康促進網－回首頁">
         </div>
